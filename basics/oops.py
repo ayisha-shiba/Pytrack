93389ed317a97172b8e51b2647ae732c1acf8561
ayisha-shiba <ayishashiba.p@gmail@example.com>
2025-10-11 18:13:12 +0530

    add basic python dir

---
+++ basics/oops.py
@@ -0,0 +1,10 @@
+# Day 1: Simple Python program
+
+# Ask the user for their name
+name = input("Enter your name: ")
+
+# Ask the user for their age
+age = input("Enter your age: ")
+
+# Print a message using their name and age
+print("Hello " + name + "! You are " + age + " years old.")
