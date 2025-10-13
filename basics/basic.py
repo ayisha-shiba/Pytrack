436b0eaca5eda98f157389a81ce386e4eb43a461
ayisha-shiba <ayishashiba.p@gmail.com>
2025-10-13 23:48:52 +0530

    Added day1_input example

--- basics/basic.py
+++ basics/basic.py
@@ -8,3 +8,9 @@ age = input("Enter your age: ")
 
 # Print a message using their name and age
 print("Hello " + name + "! You are " + age + " years old.")
+a = 10
+b = 5
+sum = a + b
+print("Sum:", sum)
+name = input("Enter your name: ")
+print("Hi", name + "! Nice to meet you.")
---
+++ basics/day1_input
@@ -0,0 +1,8 @@
+# Accept input from the user
+ch = input("Enter a character: ")
+
+# Take only the first character if user enters more than one
+ch = ch[0]  
+
+# Display the character
+print("You entered:", ch)
